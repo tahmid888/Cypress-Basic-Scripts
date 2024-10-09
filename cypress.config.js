bb211b4a4a511c7c10f790fd516fc4d008eb07a3
Tahamidul Haque <tahmidulanon@gmail.com>
2024-10-10 00:31:51 +0600

    Up-CI-CD

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true, // enable cypress to turn on video while generating html report by mochawesome
+
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
+    baseUrl:
+      "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
   env: {
